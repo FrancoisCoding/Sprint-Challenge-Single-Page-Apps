073999652aa3d76a2dd21d03b498260909ff2a10
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-26 11:38:19 -0400

    Displayed character data styled on page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard(props) {
+  console.log("characterCard props", props.data);
+  return (
+    <div>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={props.data.image}
+          alt="Rick and Morty Character"
+        />
+        <CardBody>
+          <CardTitle>Name : {props.data.name}</CardTitle>
+          <CardSubtitle>
+            Gender : {props.data.gender} Species : {props.data.species}
+          </CardSubtitle>
+          <CardText>{props.data.status}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
